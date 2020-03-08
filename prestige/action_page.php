5338287b7f140b4e6e275e089b47e71947e10e2a
Iliyas <elarifi@union.edu>
2020-03-08 16:16:42 -0400

    changes

---
+++ prestige/action_page.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<?php
+$con = mysqli_connect("localhost", "root", ""); // Establishing Connection with Server
+session_start();
+
+if(!$con){
+    die('Could not connect: ' . mysqli_error());
+}
+
+mysqli_select_db($con,"prestige-worldwide");
+
+$sql="INSERT INTO newsletter (Name, email) VALUES ('$_POST[name]', '$_POST[mail]')";
+
+if (!mysqli_query($con,$sql))
+{
+    die('Error: ' . mysqli_error());
+}
+echo "1 record added";
+mysqli_close($con)
+?>
+</body>
+</html>
